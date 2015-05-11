d38445fee6b41f9ed3b93345d9956129d75cdc9e
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-05-11 14:09:59 +0200

    add firstname validate fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   validates :lastname, presence: true, allow_blank: false
+  validates :firstname, presence: true, allow_blank: false
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
