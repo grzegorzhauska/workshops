002505f09460be2dafb9ffbf22babd378c354f07
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-05-11 14:07:55 +0200

    add first and last names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  validates :lastname, presence: true, allow_blank: false
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,4 +13,7 @@ class User < ActiveRecord::Base
   def admin?
     admin
   end
+
+
+
 end
---
+++ db/migrate/20150511115347_add_lastname_firstname_to_users.rb
@@ -0,0 +1,6 @@
+class AddLastnameFirstnameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :lastname, :string
+    add_column :users, :firstname, :string
+  end
+end
