78256fab1710435fdb564c616e2dd6693677491b
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-05-12 16:42:43 +0200

    remove validate presence of price

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,18 @@ class Product < ActiveRecord::Base
   validates :title, format: { with: /\A[a-zA-Z]+\z/,
     message: "Only letters allowed" }, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, :numericality => true,
+    :format => { :with => /A\d{1,4}(\.\d{0,2})?Z/ }
 
+  def average_rating
+    result = 0
+    reviews.each {
+      | r | result += r.rating
+    }
+    result / reviews.length
+
+    binding.pry
+  end
 
   belongs_to :category
   belongs_to :user
