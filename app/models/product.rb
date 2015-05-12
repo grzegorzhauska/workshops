9f4c689cf6a7c81110337f64f9315d363de35952
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-05-12 16:13:08 +0200

    validate presence of description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, format: { with: /\A[a-zA-Z]+\z/,
     message: "Only letters allowed" }
+  validates :description, presence: true
 
   belongs_to :category
   belongs_to :user
