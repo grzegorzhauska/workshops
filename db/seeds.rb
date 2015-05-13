04a34599af4d92f848336283340ea18e75782216
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-05-13 19:04:58 +0200

    add seed products, reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 5.times do |i|
-  User.create({ firstname: "Chicago#{i}", lastname: "Kowalski"  })
+  User.create!({ firstname: "Chicago#{i}", lastname: "Kowalski#{i}", email: "kowalski#{i}@gmail.com", password: "kowalski#{i}"  })
+end
+admin = User.last
+admin.admin = true
+admin.save
+
+category = Category.create!({name: 'kurtki'})
+
+5.times do |i|
+  Product.create!({title: "kurtka", description: "bardzo ladna kurtka", price: i, category: category, user: admin})
+end
+
+product = Product.first
+
+5.times do |i|
+  Review.create!({content: "bardzo mi sie podoba", rating: 4, user: admin, product: product})
 end
