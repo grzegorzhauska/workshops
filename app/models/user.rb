09001d0c034454b4976cc2177871e46a7c430913
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-05-13 19:11:11 +0200

    remove password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :lastname, presence: true, allow_blank: false
   validates :firstname, presence: true, allow_blank: false
   validates :email, presence: true, allow_blank: false
-  validates :password, presence: true, allow_blank: false
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
