34d006ff3beff6272aa8101358f132ebd7684c45
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-06-17 20:56:02 +0200

    fix lasst review per user

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,7 @@
 module UserHelper
 
-  def get_last_reviews
-    Review.all.order('created_at desc').take(5)
+  def get_last_reviews(user)
+    Review.where(user: user).order('created_at desc').take(5)
   end
 
   def product_count
