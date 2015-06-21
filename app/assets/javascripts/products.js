aa5e3c3dfb86101e466169ddb54b04cf752e735b
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-06-21 11:07:01 +0200

    hide revievs form

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+
+ $('#review_form').hide();
+
+  $('.show_review_form').click(function() {
+   $('#review_form').show();
+   $('.show_review_form').hide();
+  });
+  
+ });
